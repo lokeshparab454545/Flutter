5b9ac71fbcd4101870d14838ccaedef84db85bcd
Lokesh Parab <lokeshparab4545@gmail.com>
2022-07-02 20:11:57 +0530

    day 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -37,43 +37,95 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: Colors.red,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: Container(
+                  color: Colors.amber,
+                  padding: const EdgeInsets.all(10),
+                  child: Row(children: [
+                    const CircleAvatar(
+                      radius: 40,
+                      backgroundImage: NetworkImage(
+                          "https://expletivearchive.com/wp-content/uploads/2020/07/mGJ9St.jpg"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Talha Anjum",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ]),
+                ),
+              ),
+              const ListTile(
+                leading: Icon(Icons.folder),
+                title: Text("My files"),
+                iconColor: Colors.amber,
+              ),
+              const ListTile(
+                leading: Icon(Icons.person),
+                title: Text("My Profile"),
+                iconColor: Colors.amber,
+              ),
+              const ListTile(
+                leading: Icon(Icons.group),
+                title: Text("My Shared with me"),
+                iconColor: Colors.amber,
+              ),
+              const ListTile(
+                leading: Icon(Icons.star),
+                title: Text("My Favourites"),
+                iconColor: Colors.amber,
+              ),
+              const ListTile(
+                leading: Icon(Icons.delete),
+                iconColor: Colors.amber,
+                title: Text("My Trash"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.upload),
+                title: Text("My Uploads"),
+                iconColor: Colors.amber,
+              ),
+              const Divider(),
+              const ListTile(
+                leading: Icon(Icons.share),
+                title: Text("Share"),
+                iconColor: Colors.amber,
+              ),
+              const ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout"),
+                iconColor: Colors.amber,
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: const Text("Dismisible Widget"),
         backgroundColor: Colors.red,
         elevation: 3,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final fruit = fruits[index];
-          return Dismissible(
-            key: Key(fruit),
-            onDismissed: (direction) {
-              if (direction == DismissDirection.startToEnd) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(fruits[index]),
-                    backgroundColor: Colors.red,
-                  ),
-                );
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(fruits[index]),
-                    backgroundColor: Colors.green,
-                  ),
-                );
-              }
-            },
-            background: Container(color: Colors.red),
-            secondaryBackground: Container(color: Colors.green),
-            child: Card(
-              child: ListTile(
-                title: Text(fruits[index]),
-              ),
-            ),
-          );
-        },
-        itemCount: fruits.length,
+      body: Container(
+        child: const Center(
+          child: Text("Hi Lokesh!"),
+        ),
       ),
     );
   }
